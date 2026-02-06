36d34a11f610d10958905d2769845567548eb19f
abdelrahman elsayed attia <166997051+Abdoh1100@users.noreply.github.com>
2026-02-06 10:23:49 +0100

    state and full showDescription button
    adding the state to toggle the description

--- React-jobs/src/components/Job.jsx
+++ React-jobs/src/components/Job.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 const job = ({jobData}) => {
+
+  const [showFullDescription,setShowFullDescription] =useState(false)
+
+  let description = jobData.description;
+
+  if(!showFullDescription){
+    description = jobData.description.substring(0, 20) + "..."
+  }
+
   return (
     <div className="rounded-xl bg-white p-6 shadow-md ring-1 ring-black/5">
       <div className="text-sm text-gray-500">{jobData.type}</div>
 
       <h3 className="mt-2 text-lg font-semibold text-gray-900">{jobData.title}</h3>
 
-      <p className="mt-3 text-sm leading-6 text-gray-600">{jobData.description}</p>
+      <p className="mt-3 text-sm leading-6 text-gray-600">{description}</p>
+      <button onClick={() => setShowFullDescription((prevState)=>!prevState)} className="text-sm text-indigo-600 hover:text-indigo-800">
+        {showFullDescription ? "Less" : "More"}
+      </button>
 
       <div className="mt-5 space-y-1">
         <div className="text-sm font-semibold text-indigo-700">{jobData.salary}</div>
