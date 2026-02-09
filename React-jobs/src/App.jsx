722fdac9e91502ef1a7f8de9191c7c701e623e23
abdelrahman elsayed attia <166997051+Abdoh1100@users.noreply.github.com>
2026-02-10 00:49:59 +0100

    day3
    add job page
    new technologies and more

--- React-jobs/src/App.jsx
+++ React-jobs/src/App.jsx
@@ -1,29 +1,38 @@
-import { Route,createBrowserRouter,RouterProvider,createRoutesFromElements}from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import MainLayout from './layouts/MainLayout'
-import JobsPage from './pages/JobsPage'
-import NotFoundPage from './pages/NotFoundPage'
-import TestPage from './pages/TestPage'
-import JobPage,{jobLoader} from './pages/JobPage'
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import TestPage from "./pages/TestPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJob from "./pages/AddJob";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout/>}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='*' element={<NotFoundPage />} />
-    <Route path='/test' element={<TestPage />} />
-    <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/>
-  </Route>
-  
-  )
-)
 const App = () => {
+  const add = (newJob) => {
+    console.log(newJob);
+  };
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="/add-job" element={<AddJob addJobSubmit={add} />} />
+      </Route>,
+    ),
+  );
   return (
-  <>
-  <RouterProvider router={router} />
-  </>
-  )
-}
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
 
-export default App
+export default App;
--- React-jobs/src/pages/JobPage.jsx
+++ React-jobs/src/pages/JobPage.jsx
@@ -1,52 +1,49 @@
-import React from 'react'
-import {useState,useEffect} from 'react'
-import { useParams, useLoaderData} from 'react-router-dom';
-import JobShape from '../components/jobShape';
-import Job from '../components/Job';
-import Spinner from '../components/Spinner';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import JobShape from "../components/jobShape";
+import Job from "../components/Job";
+import Spinner from "../components/Spinner";
 
 const JobPage = () => {
-    
-    //in the params you have to destructure the id from the url 
-    // const {id} = useParams();
-    const job=useLoaderData();
-    // const [loading,setLoading]=useState(true)
-    //data Fetching again 
-    // const [job,setJob]=useState(null);
-    // useEffect(()=>{
-    //     const fetchJob=async ()=>{
-    //         try{
-    //             const res=await fetch(`/api/jobs/${id}`)
-    //             const jobById=await res.json();
-    //             console
-    //             setJob(jobById);
-    //         }catch(Error){
-    //             console.log("Error : ",Error);
-    //         }finally{
-    //             setLoading(false)
-    //         }
-    //     }
-    //     fetchJob();
-    // },[])
-
+  //in the params you have to destructure the id from the url
+  // const {id} = useParams();
+  const job = useLoaderData();
+  // const [loading,setLoading]=useState(true)
+  //data Fetching again
+  // const [job,setJob]=useState(null);
+  // useEffect(()=>{
+  //     const fetchJob=async ()=>{
+  //         try{
+  //             const res=await fetch(`/api/jobs/${id}`)
+  //             const jobById=await res.json();
+  //             console
+  //             setJob(jobById);
+  //         }catch(Error){
+  //             console.log("Error : ",Error);
+  //         }finally{
+  //             setLoading(false)
+  //         }
+  //     }
+  //     fetchJob();
+  // },[])
 
   return (
     <>
-    <JobShape job={job}/>
-    {/* {loading?<Spinner/>:
+      <JobShape job={job} />
+      {/* {loading?<Spinner/>:
     <div 
     className="mt-8 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {console.log("your job : ",job)}
           <Job jobData={job}/>
           </div>
     } */}
-    
     </>
-  )
-}
-const jobLoader=async({params})=>{
-    const res=await fetch(`/api/jobs/${params.id}`);
-    const data=await res.json();
-    return data;
+  );
 };
-export {JobPage as default,jobLoader};
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+// eslint-disable-next-line react-refresh/only-export-components
+export { JobPage as default, jobLoader };
